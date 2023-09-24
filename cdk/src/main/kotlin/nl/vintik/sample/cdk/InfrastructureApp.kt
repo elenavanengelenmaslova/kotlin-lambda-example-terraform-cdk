818be4afc7a36973461ac5887913037ca01824dc
elena.vanengelen <elena.vanengelen@edsn.nl>
2023-09-24 10:18:35 +0200

    increase memory & improve name stack

--- cdk/src/main/kotlin/nl/vintik/sample/cdk/InfrastructureApp.kt
+++ cdk/src/main/kotlin/nl/vintik/sample/cdk/InfrastructureApp.kt
@@ -4,6 +4,6 @@ import com.hashicorp.cdktf.App
 
 fun main() {
     val app = App()
-    InfrastructureJvmArm64Stack(app, "Terraform-Cdk-Kotlin-Lambda-JVM-Arm64-example")
+    InfrastructureJvmArm64Stack(app, "Terraform-Cdk-Kotlin-Lambda-JVM-Arm64-Example")
     app.synth()
 }
